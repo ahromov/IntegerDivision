22e11235574cf132f9408cd7e6177387eb31d275
Andrii Hromov <andrii.hromov@gmail.com>
2018-04-14 13:09:27 +0300

    Reworked project according to mentors review.

--- src/anagram/Main.java
+++ src/anagram/Main.java
@@ -15,54 +15,55 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-		if (args.length > 0) {
-			reverseEachWordOfString(args[0]);
-		} else {
-			System.out.println("No argument! USAGE: java Anagram \"Some line\"");
-		}
-	}
+        if (args.length > 0) {
+            reverseEachWordOfString(args[0]);
+        } else {
+            System.out.println("No argument! USAGE: java Anagram \"Some line\"");
+        }
+    }
 
-	public static void reverseEachWordOfString(String input) {
-		String[] words = input.split(" ");
-		List<String> wordsList = Arrays.asList(words);
-		StringBuilder newLineBuffer = new StringBuilder("");
-		String reverseString = parseEachWordsOnCharsAndPuttReverseIntoArray(wordsList, newLineBuffer);
-		System.out.println(input);
-		System.out.println(reverseString);
-	}
+    public static void reverseEachWordOfString(String input) {
+        String[] words = input.split(" ");
+        List<String> wordsList = Arrays.asList(words);
+        StringBuilder newLineBuffer = new StringBuilder("");
+        String reverseString = parseEachWordsOnCharsAndPuttReverseIntoArray(wordsList, newLineBuffer);
+        System.out.println(input);
+        System.out.println(reverseString);
+    }
 
-	private static String parseEachWordsOnCharsAndPuttReverseIntoArray(List<String> words, StringBuilder reverseString) {
-		for (int i = 0; i < words.size(); i++) {
-			String word = words.get(i);
-			char[] arrayForNewReverseWord = parseWordOnChars(word);
-			reverseEachWord(word, arrayForNewReverseWord);
-			reverseString.append(String.valueOf(arrayForNewReverseWord)).append(" ");
-		}
-		return reverseString.toString();
-	}
+    private static String parseEachWordsOnCharsAndPuttReverseIntoArray(List<String> words, StringBuilder reverseString) {
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+            char[] arrayForNewReverseWord = parseWordOnChars(word);
+            reverseEachWord(word, arrayForNewReverseWord);
+            reverseString.append(String.valueOf(arrayForNewReverseWord)).append(" ");
+        }
+        return reverseString.toString();
+    }
 
-	private static void reverseEachWord(String word, char[] arrayForNewReverseWord) {
-		for (int j = 0; j < word.length(); j++) {
-			if (Character.isLetter(word.charAt(j))) {
-				for (int k = arrayForNewReverseWord.length - 1; k >= 0; k--) {
-					if (arrayForNewReverseWord[k] == '\u0000') {
-						arrayForNewReverseWord[k] = word.charAt(j);
-						break;
-					}
-				}
-			}
-		}
-	}
+    private static void reverseEachWord(String word, char[] arrayForNewReverseWord) {
+        for (int j = 0; j < word.length(); j++) {
+            if (Character.isLetter(word.charAt(j))) {
+                for (int k = arrayForNewReverseWord.length - 1; k >= 0; k--) {
+                    if (arrayForNewReverseWord[k] == '\u0000') {
+                        arrayForNewReverseWord[k] = word.charAt(j);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
-	private static char[] parseWordOnChars(String word) {
-		char[] arrayForNewReverseWord = new char[word.length()];
-		for (int j = 0; j < word.length(); j++) {
-			if (Character.isLetter(word.charAt(j))) {
-				arrayForNewReverseWord[j] = '\u0000';
-			} else
-				arrayForNewReverseWord[j] = word.charAt(j);
-		}
-		return arrayForNewReverseWord;
-	}
-    
+    private static char[] parseWordOnChars(String word) {
+        char[] arrayForNewReverseWord = new char[word.length()];
+        for (int j = 0; j < word.length(); j++) {
+            if (Character.isLetter(word.charAt(j))) {
+                arrayForNewReverseWord[j] = '\u0000';
+            } else {
+                arrayForNewReverseWord[j] = word.charAt(j);
+            }
+        }
+        return arrayForNewReverseWord;
+    }
+
 }
