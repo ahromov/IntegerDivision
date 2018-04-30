efd23d5b16c8e0d19538528b5105b6838b0f7383
Andrew <Andrew@DESKTOP-455BKR8>
2018-04-30 20:50:13 +0300

    Reworked.

--- test/anagram/AnagramNullTest.java
+++ test/anagram/AnagramNullTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
  */
 public class AnagramNullTest {
     
-    protected Anagram anagram;
+    private final Anagram anagram;
     
     public AnagramNullTest() {
         this.anagram = new Anagram();
